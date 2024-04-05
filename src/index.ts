f236f0930dc22860508b81922480628b097a2ab9
mickeymarse <mmserrapica@tutanota.com>
2024-04-05 16:08:39 +0100

    Rendered README file as landing HTML page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import { Elysia } from 'elysia';
+import { html } from '@elysiajs/html';
+
+import remarkHtml from 'remark-html';
+import remarkParse from 'remark-parse';
+import { read } from 'to-vfile';
+import { unified } from 'unified';
+
 import { getAllTarots, getRandomTarot } from './scripts/tarot';
 
+const readme = await unified()
+  .use(remarkParse)
+  .use(remarkHtml)
+  .process(await read('README.md'));
+
 const app = new Elysia()
-  .get('/', () => 'Hello Elysia')
+  .use(html())
+  .get(
+    '/',
+    () =>
+      `<html lang="en">
+  <body>  
+  ${readme}
+    </body>
+    </html>`
+  )
   .get('/tarots/all', async () => {
     const data = await getAllTarots();
     console.log(data);
